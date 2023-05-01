4e8554689dd1f777b212a4bd7914357e7ac0ec4d
viniAmorim <amorimm.viniciuss@gmail.com>
2023-05-01 15:51:23 -0300

    fibonacci sequence

---
+++ fib/fibMemo.js
@@ -0,0 +1,11 @@
+// js object, keys will be arg to fn, value will be the return value
+const fib = (n, memo = {}) => {
+    if (n in memo) return memo[n];
+    if (n <= 2) return 1;
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+};
+
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(50));
